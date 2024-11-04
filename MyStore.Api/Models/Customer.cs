1d0da2dd4842d16d60afb45cc7a0f819b96e859f
Z3th <sethanmawen97@gmail.com>
2024-11-04 22:44:38 +0000

    Adjustments to fix bugs

--- MyStore.Api/Models/Customer.cs
+++ MyStore.Api/Models/Customer.cs
@@ -6,17 +6,41 @@ namespace MyStore.Api.Models
     public class Customer
     {
         [Key]
-        public string CustomerNumber { get; set; }
+        public string? CustomerNumber { get; set; } // Made nullable
         [Required]
         [MaxLength(100)]
-        public string Name { get; set; }
+        public string? Name { get; set; } // Made nullable
         [MaxLength(255)]
-        public string Description { get; set; }
+        public string? Description { get; set; } // Made nullable
         [MaxLength(100)]
-        public string ContactInformation { get; set; }
+        public string? ContactInformation { get; set; } // Made nullable
         [Required]
         [Range(0, double.MaxValue)]
         public decimal CurrentBalance { get; set; }
         public DateTime? LastTransactionDate { get; set; }
     }
 }
+
+
+// using System;
+// using System.ComponentModel.DataAnnotations;
+
+// namespace MyStore.Api.Models
+// {
+//     public class Customer
+//     {
+//         [Key]
+//         public string CustomerNumber { get; set; }
+//         [Required]
+//         [MaxLength(100)]
+//         public string Name { get; set; }
+//         [MaxLength(255)]
+//         public string Description { get; set; }
+//         [MaxLength(100)]
+//         public string ContactInformation { get; set; }
+//         [Required]
+//         [Range(0, double.MaxValue)]
+//         public decimal CurrentBalance { get; set; }
+//         public DateTime? LastTransactionDate { get; set; }
+//     }
+// }
--- MyStore.Api/Program.cs
+++ MyStore.Api/Program.cs
@@ -1,9 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration; // For accessing configuration settings
 using MyStore.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure the database context
 builder.Services.AddDbContext<ShopDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+app.Run();
